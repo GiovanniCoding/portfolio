b6bce0813d29be1ab81f025b18f784c03c7fab32
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-06-24 01:57:09 -0500

    default export correction

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -2,6 +2,8 @@ import { AppRouter } from "./router/AppRouter"
 
 export const Portfolio = () => {
   return (
-    <AppRouter />
+    <>
+        <AppRouter />
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter } from "react-router-dom";
 
-import Portfolio from './Portfolio'
+import { Portfolio } from './Portfolio'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
       <Portfolio />
     </BrowserRouter>
-  </React.StrictMode>
 )
