b2a773415b35a6bddecbd4a2cd0501fd4cb53f4b
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-06-24 01:49:05 -0500

    Basic Router Structure added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter } from "react-router-dom";
+
 import Portfolio from './Portfolio'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Portfolio />
+    <BrowserRouter>
+      <Portfolio />
+    </BrowserRouter>
   </React.StrictMode>
 )
